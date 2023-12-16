bc04d28dee61ec059a978bc2d77f20146d861faf
Mohan Kadu <mohankadu22@gmail.com>
2023-12-16 15:29:58 +0530

    created sample scrollable list and changed color

--- app/src/main/java/com/example/composelearn/MainActivity.kt
+++ app/src/main/java/com/example/composelearn/MainActivity.kt
@@ -1,15 +1,34 @@
 package com.example.composelearn
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composelearn.ui.theme.ComposeLearnTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +36,187 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeLearnTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = List(1000) { "$it" }
+) {
+    LazyColumn(
         modifier = modifier
-    )
+            .padding(vertical = 4.dp)
+    ) {
+        items(names) { name ->
+            Greeting(name = name)
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(
+    name = "Light Mode",
+    widthDp = 320,
+    showBackground = true,
+)
+@Preview(
+    name = "Dark Mode",
+    widthDp = 320,
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+@Composable
+fun PreviewGreetings() {
+    ComposeLearnTheme {
+        Greetings()
+    }
+}
+
+@Composable
+fun MyApp(
+    modifier: Modifier = Modifier,
+) {
+    var shouldShowOnboarding by rememberSaveable {
+        mutableStateOf(true)
+    }
+
+    Surface(modifier = modifier) {
+        if (shouldShowOnboarding) {
+            OnboardingScreen(onContinueClicked = {
+                shouldShowOnboarding = false
+            })
+        } else {
+            Greetings()
+        }
+    }
+}
+
+@Preview(
+    name = "Light Mode",
+    showBackground = true,
+    widthDp = 320,
+)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode",
+    widthDp = 320,
+)
+@Composable
+fun PreviewMyApp() {
+    ComposeLearnTheme {
+        MyApp(modifier = Modifier.fillMaxSize())
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    var expanded by rememberSaveable {
+        mutableStateOf(false)
+    }
+    val extraPadding by animateDpAsState(
+        targetValue = if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        ),
+        label = "Extra Padding",
+    )
+
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier
+            .padding(
+                vertical = 4.dp,
+                horizontal = 8.dp,
+            )
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(
+                        bottom = extraPadding.coerceAtLeast(0.dp)
+                    )
+            ) {
+                Text(
+                    text = "Hello,",
+                )
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.headlineMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
+            }
+            ElevatedButton(
+                onClick = {
+                    expanded = !expanded
+                }
+            ) {
+                Text(text = if (expanded) "Show less" else "Show more")
+            }
+        }
+    }
+}
+
+@Preview(
+    name = "Light Mode",
+)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    name = "Dark Mode"
+)
 @Composable
 fun GreetingPreview() {
     ComposeLearnTheme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun OnboardingScreen(
+    modifier: Modifier = Modifier,
+    onContinueClicked: () -> Unit,
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Welcome to the Basic Compose Codelab!")
+        Button(
+            modifier = Modifier.padding(16.dp),
+            onClick = onContinueClicked
+        ) {
+            Text(text = "Continue")
+        }
+    }
+}
+
+@Preview(
+    heightDp = 320,
+    widthDp = 320,
+    showBackground = true,
+    name = "Light Mode",
+)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    heightDp = 320,
+    widthDp = 320,
+    name = "Dark Mode"
+)
+@Composable
+fun PreviewOnboardingScreen() {
+    ComposeLearnTheme {
+        OnboardingScreen(onContinueClicked = {
+            // Do nothing on click
+        })
     }
 }
